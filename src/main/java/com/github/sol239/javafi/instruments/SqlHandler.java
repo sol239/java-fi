f28c50ce8f381606281e97199fef2ef10d51510b
David Valek <david.valek17@gmail.com>
2025-02-21 17:20:21 +0100

    Add RSI sql query

--- src/main/java/com/github/sol239/javafi/instruments/SqlHandler.java
+++ src/main/java/com/github/sol239/javafi/instruments/SqlHandler.java
@@ -2,20 +2,34 @@ package com.github.sol239.javafi.instruments;
 
 import com.github.sol239.javafi.postgre.DBHandler;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SqlHandler {
 
+    /**
+     * Executes a SQL query
+     * @param query the query to be executed
+     */
     public static void executeQuery(String query) {
-        // execute query
         DBHandler dbHandler = new DBHandler();
         dbHandler.executeQuery(query);
         dbHandler.closeConnection();
     }
 
-    public static String[] getAllSqlInstruments() {
-        //TODO: implement this method and even in server
-        return new String[] {
-                "SimpleMovingAverage(String tableName, int period)",
-        };
+    /**
+     * Returns all the SQL instruments available
+     * @return an array with all the SQL instruments available
+     */
+    public static List<String> getAllSqlInstruments() {
+
+        List<String> instruments = new ArrayList<>();
+
+        // TODO: implement the logic
+
+        return instruments;
+
     }
 
 
--- src/main/java/com/github/sol239/javafi/instruments/SqlInstruments.java
+++ src/main/java/com/github/sol239/javafi/instruments/SqlInstruments.java
@@ -1,7 +1,6 @@
 package com.github.sol239.javafi.instruments;
 
 import com.github.sol239.javafi.postgre.DBHandler;
-
 import java.lang.reflect.Array;
 
 public class SqlInstruments {
@@ -26,6 +25,49 @@ public class SqlInstruments {
                 period, period - 1, period, tableName, // CTE (Common Table Expression)
                 tableName, period, period, period, period, // UPDATE statement
                 tableName, period // WHERE clause
+        );
+
+        return query;
+    }
+
+    public String rsi(String tableName, int period) {
+        String query = String.format(
+                "ALTER TABLE %s DROP COLUMN IF EXISTS rsi%d; " + // Drop the column if it exists
+                        "ALTER TABLE %s ADD COLUMN rsi%d DOUBLE PRECISION; " + // Add the column
+                        "WITH price_changes AS ( " +
+                        "    SELECT date, close, " +
+                        "           LAG(close) OVER (ORDER BY date) AS prev_price " +
+                        "    FROM %s " +
+                        "), " +
+                        "gains_losses AS ( " +
+                        "    SELECT date, close, " +
+                        "           CASE WHEN close - prev_price > 0 THEN close - prev_price ELSE 0 END AS gain, " +
+                        "           CASE WHEN close - prev_price < 0 THEN ABS(close - prev_price) ELSE 0 END AS loss " +
+                        "    FROM price_changes " +
+                        "), " +
+                        "avg_gains_losses AS ( " +
+                        "    SELECT date, close, " +
+                        "           AVG(gain) OVER (ORDER BY date ROWS BETWEEN %d PRECEDING AND CURRENT ROW) AS avg_gain, " +
+                        "           AVG(loss) OVER (ORDER BY date ROWS BETWEEN %d PRECEDING AND CURRENT ROW) AS avg_loss " +
+                        "    FROM gains_losses " +
+                        "), " +
+                        "rsi AS ( " +
+                        "    SELECT date, close, " +
+                        "           CASE WHEN avg_loss = 0 THEN 100 " +
+                        "                ELSE 100 - (100 / (1 + (avg_gain / avg_loss))) END AS rsi%d " +
+                        "    FROM avg_gains_losses " +
+                        ") " +
+                        "UPDATE %s " +
+                        "SET rsi%d = rsi.rsi%d " +
+                        "FROM rsi " +
+                        "WHERE %s.date = rsi.date;",
+                tableName, period, // DROP COLUMN
+                tableName, period, // ADD COLUMN
+                tableName, // price_changes CTE
+                period - 1, period - 1, // avg_gains_losses CTE (ROWS BETWEEN)
+                period, // rsi CTE
+                tableName, period, period, // UPDATE statement
+                tableName // WHERE clause
         );
 
         return query;
