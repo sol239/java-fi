889b61c548710922b656559e8ac4f99558fd985e
David Valek <david.valek17@gmail.com>
2025-02-23 08:21:33 +0100

    Add more db tests

--- src/test/java/DBHandlerTest.java
+++ src/test/java/DBHandlerTest.java
@@ -2,6 +2,8 @@ import com.github.sol239.javafi.postgre.DBHandler;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class DBHandlerTest {
@@ -45,6 +47,41 @@ public class DBHandlerTest {
         List<String> tables2 = db.getAllTables();
         assert tables1.size() + 1 == tables2.size();
     }
+
+    @Test
+    public void cleanTest() {
+        // TODO: To be implemented
+    }
+
+    @Test
+    public void deleteTableTest() {
+        DBHandler db = new DBHandler();
+        List<String> tables1 = db.getAllTables();
+        db.executeQuery("CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(50))");
+        List<String> tables2 = db.getAllTables();
+        db.deleteTable("test_table");
+        List<String> tables3 = db.getAllTables();
+        assert tables1.size() == tables3.size();
+    }
+
+    @Test
+    public void checkCredentialsTest() {
+        DBHandler db = new DBHandler();
+        try {
+            List<String> config = Files.readAllLines(Paths.get(DBHandler.CONFIG_FILE_PATH));
+            db.checkCredentials(db.credentials);
+            boolean expected = true;
+            boolean actual = (config.get(0).equals(db.credentials.get(0)) && config.get(1).equals(db.credentials.get(1)) && config.get(2).equals(db.credentials.get(2)));
+            assert expected == actual;
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    // TODO: Add more tests
+
+
 
 
 }
