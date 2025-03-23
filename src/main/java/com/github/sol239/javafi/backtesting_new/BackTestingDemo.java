4c7b77ce06ebd4d74c7361aba0207fedbf6d129b
David Valek <david.valek17@gmail.com>
2025-03-23 21:45:58 +0100

    Create separate class

--- src/main/java/com/github/sol239/javafi/backtesting_new/BackTestingDemo.java
+++ src/main/java/com/github/sol239/javafi/backtesting_new/BackTestingDemo.java
@@ -12,9 +12,356 @@ import java.sql.SQLException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
+class BacktestingExecutor {
+
+    /**
+     * Suffix for the columns that store the open signals for the strategy
+     */
+    public static final String STRATEGY_OPEN_COLUMN_SUFFIX = "_stgO_";
+
+    /**
+     * Suffix for the columns that store the close signals for the strategy
+     */
+    public static final String STRATEGY_CLOSE_COLUMN_SUFFIX = "_stgC_";
+
+    public static final String DATE_RESTRICTION_PATH  = "C:\\Users\\david_dyn8g78\\PycharmProjects\\elliptic\\data-preparation\\main\\date.txt";
+
+    public static final String TRADES_RESULT_JSON_PATH = "C:\\Users\\david_dyn8g78\\PycharmProjects\\elliptic\\data-preparation\\main\\trades.json";
+
+    private List<Trade> openedTrades = new ArrayList<>();
+    private List<Trade> loosingTrades = new ArrayList<>();
+    private List<Trade> winningTrades = new ArrayList<>();
+
+    private Map<Integer, Setup> setups = new HashMap<>();
+
+    private DBHandler db;
+
+    /**
+     * Clause that defines the conditions for opening a long trade (closing a short trade)
+     * e.g. "WHERE rsi_14_ins_ <= 27"
+     */
+    public String originalOpenClause;
+
+    /**
+     * Clause that defines the conditions for closing a long trade (opening a short trade)
+     * e.g. "WHERE rsi_14_ins_ >= 70"
+     */
+    public String originalCloseClause;
+    public BacktestingExecutor(String originalOpenClause, String originalCloseClause) {
+        this.originalOpenClause = originalOpenClause;
+        this.originalCloseClause = originalCloseClause;
+
+        this.db = new DBHandler();
+        this.db.setFetchSize(1000);
+
+    }
+
+    /**
+     * Calculates the difference between two dates and returns true if the difference is greater than 'n' seconds.
+     * @param dateString1 The first date string in the format 'yyyy-MM-dd HH:mm:ss[.SSSSSS]'
+     * @param dateString2 The second date string in the format 'yyyy-MM-dd HH:mm:ss[.SSSSSS]'
+     * @param n The number of seconds that the difference must be greater than
+     * @return True if the difference between the two dates is greater than 'n' seconds, false otherwise
+     */
+    public static boolean isDifferenceGreaterThan(String dateString1, String dateString2, long n) {
+        // Define the format for the date string that handles both formats (with and without microseconds)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss[.SSSSSS]");
+
+        // Parse the strings to LocalDateTime objects
+        LocalDateTime date1 = LocalDateTime.parse(dateString1, formatter);
+        LocalDateTime date2 = LocalDateTime.parse(dateString2, formatter);
+
+        // Calculate the difference in seconds between the two dates
+        Duration duration = Duration.between(date1, date2);
+        long differenceInSeconds = Math.abs(duration.getSeconds());
+
+        // Return true if the difference in seconds is greater than 'n'
+        return differenceInSeconds > n;
+    }
+
+    /**
+     * Returns the name of the column that stores the signals for the strategy.
+     * @param open True if the column stores the open signals, false if it stores the close signals
+     * @return The name of the column that stores the signals for the strategy
+     */
+    public String getStrategyColumnName(boolean open) {
+        if (open) {
+            return "\"" + originalOpenClause + " - " + originalCloseClause + STRATEGY_OPEN_COLUMN_SUFFIX + "\"";
+        } else {
+            return "\"" + originalOpenClause + " - " + originalCloseClause + STRATEGY_CLOSE_COLUMN_SUFFIX + "\"";
+        }
+    }
+
+    /**
+     * Creates the columns in the database table that store the signals for the strategy.
+     * @param tableName The name of the database table
+     */
+    public void createStrategyColumns(String tableName) {
+        String openColumnName = getStrategyColumnName(true);
+        String closeColumnName = getStrategyColumnName(false);
+
+        // Creating open/false boolean columns
+        String createOpenColumn = """
+                ALTER TABLE IF EXISTS %s
+                ADD COLUMN IF NOT EXISTS %s BOOLEAN DEFAULT FALSE;
+                """.formatted(tableName, openColumnName);
+
+        String createCloseColumn = """
+                ALTER TABLE IF EXISTS %s
+                ADD COLUMN IF NOT EXISTS %s BOOLEAN DEFAULT FALSE;
+                """.formatted(tableName, closeColumnName);
+
+        db.executeQuery(createOpenColumn);
+        db.executeQuery(createCloseColumn);
+        System.out.println("Strategy columns created.");
+    }
+
+    /**
+     * Updates the columns in the database table that store the signals for the strategy.
+     * @param tableName The name of the database table
+     */
+    public void updateStrategyColumns(String tableName) {
+        String openColumnName = getStrategyColumnName(true);
+        String closeColumnName = getStrategyColumnName(false);
+
+        // Update open/close columns based on the original clauses
+        String updateOpenColumn = """
+                UPDATE %s
+                SET %s = TRUE
+                %s;
+                """.formatted(tableName, openColumnName, originalOpenClause);
+
+        String updateCloseColumn = """
+                UPDATE %s
+                SET %s = TRUE
+                %s;
+                """.formatted(tableName, closeColumnName, originalCloseClause);
+
+        db.executeQuery(updateOpenColumn);
+        db.executeQuery(updateCloseColumn);
+        System.out.println("Strategy columns updated.");
+    }
+
+    public void run(String tableName, Setup setup) {
+
+        long t1 = System.currentTimeMillis();
+
+        List<String> lines;
+        String dateRestriction;
+        try {
+            lines = Files.readAllLines(Path.of(DATE_RESTRICTION_PATH));
+            dateRestriction = lines.get(0);
+        } catch (IOException e) {
+            lines = null;
+            dateRestriction = "";
+        }
+
+        ResultSet rs;
+        try {
+            rs = this.db.getResultSet(String.format("SELECT * FROM %s %s ORDER BY id", tableName, dateRestriction));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+
+        String openX = this.getStrategyColumnName(true).substring(1, this.getStrategyColumnName(true).length() - 1);
+        String closeX = this.getStrategyColumnName(false).substring(1, this.getStrategyColumnName(false).length() - 1);
+
+        int skipRows = 1;
+        int c = 0;
+        try {
+            while (rs.next()) {
+
+
+                c++;
+
+                if (c < skipRows) {
+                    continue;
+                }
+
+                boolean open = rs.getBoolean(openX);
+                boolean close = rs.getBoolean(closeX);
+                double closePrice = rs.getDouble("close");
+                String now = rs.getString("date");
+
+
+                // trade closing
+                Iterator<Trade> iterator = this.openedTrades.iterator();
+                while (iterator.hasNext()) {
+                    Trade trade = iterator.next();
+                    boolean tradeClosed = false;
+
+                    if (closePrice <= trade.stopPrice) {
+                        tradeClosed = true;
+                    }
+
+                    // It is generally good to specify that:
+                    // - current close must be > than trade.open * (1 + takeProfit)
+                    // - current close must be > than trade.open * (1 + fee)
+                    if (close) {
+                        tradeClosed = true;
+                    }
+
+                    if (tradeClosed && closePrice >= trade.openPrice * (1 + setup.fee)) {
+                        trade.closeTime = rs.getString("date");
+                        trade.closePrice = closePrice;
+                        trade.PnL = (trade.closePrice * trade.amount - trade.openPrice * trade.amount) * setup.leverage * (1 - setup.fee);
+                        if (closePrice >= trade.openPrice) {
+                            //System.out.println("WINNING TRADE");
+                            winningTrades.add(trade);
+                        } else {
+                            //System.out.println("LOOSING TRADE");
+                            loosingTrades.add(trade);
+                        }
+                        iterator.remove();
+
+                        double profit = (trade.closePrice * trade.amount - trade.openPrice * trade.amount) * setup.leverage;
+
+                        setup.balance += profit;
+                        // System.out.println("Balance: " + balance);
+
+                    }
+
+                }
+
+                // long trade opening
+                if (open && this.openedTrades.size() < setup.maxTrades && setup.balance - setup.amount >= 0) {
+                    if (!this.openedTrades.isEmpty()) {
+                        if (isDifferenceGreaterThan(now,this.openedTrades.get(this.openedTrades.size() - 1).openTime, setup.delaySeconds)) {
+                            Trade newTrade = new Trade(closePrice, closePrice * (1 + setup.takeProfit), closePrice * (1 - setup.stopLoss), 0, (setup.amount * setup.leverage / closePrice), tableName, rs.getString("date"), "");
+                            setup.balance -= setup.amount / closePrice;
+                            //System.out.println("Balance: " + balance);
+                            this.openedTrades.add(newTrade);
+
+
+                        }
+                    } else  {
+                        Trade newTrade = new Trade(closePrice, closePrice * (1 + setup.takeProfit), closePrice * (1 - setup.stopLoss), 0, (setup.amount * setup.leverage / closePrice), tableName, rs.getString("date"), "");
+                        setup.balance -= setup.amount / closePrice;
+                        //System.out.println("Balance: " + balance);
+                        this.openedTrades.add(newTrade);
+                    }
+
+                    if (this.openedTrades.size() > setup.maxOpenedTrades) {
+                        setup.maxOpenedTrades = this.openedTrades.size();
+                    }
+
+                    /*
+                    System.out.println("-----------------------------------------");
+                    System.out.println("CLOSE PRICE: " + closePrice + " | " + rs.getString("rsi_14_ins_"));
+                    System.out.println("Trade opened: ");
+                    System.out.println(newTrade);
+                    System.out.println("-----------------------------------------");
+                    */
+                }
+
+                // short trade opening
+                // TODO
+
+
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        long t2 = System.currentTimeMillis();
+
+
+
+        List<Trade> allTrades = evaluateTrades(winningTrades, loosingTrades, setup.amount, setup.leverage, setup.fee);
+        // Store into table
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Convert list of trades to JSON and write to file
+            File outputFile = new File(TRADES_RESULT_JSON_PATH);
+
+            objectMapper.writeValue(outputFile, allTrades);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\n****************************************");
+        System.out.println("Balance: " + String.format("%.2f", setup.balance) + " USD");
+        System.out.println("Max opened trades: " + setup.maxOpenedTrades);
+        System.out.println("EXECUTION TIME: " + Double.parseDouble(String.valueOf(t2 - t1)) / 1000 + " s");
+        System.out.println("****************************************");
+    }
+
+    public static List<Trade> evaluateTrades(List<Trade> winningTrades, List<Trade> loosingTrades, double tradeSize, double leverage, double fee) {
+        long winningTradesCount = winningTrades.size();
+        long loosingTradesCount = loosingTrades.size();
+        double winRate = (double) winningTradesCount / (winningTradesCount + loosingTradesCount);
+
+        // Displaying trades
+
+        List<Trade> allTrades = new ArrayList<>();
+        allTrades.addAll(winningTrades);
+        allTrades.addAll(loosingTrades);
+
+
+        double percentProfitSum = 0;
+        for (Trade trade : allTrades) {
+            // print % profit
+            double profitPercent = (((trade.closePrice) / trade.openPrice) * 100) - 100;
+            //System.out.println(trade);
+            System.out.println("Profit: " + String.format("%.2f", profitPercent) + "%");
+        }
+        double _profit = 0;
+        for (Trade trade : allTrades) {
+            _profit += (trade.closePrice * trade.amount - trade.openPrice * trade.amount) * leverage * (1 - fee);
+        }
+
+
+        System.out.println("****************************************");
+        System.out.println("Winning trades: " + winningTradesCount);
+        System.out.println("Loosing trades: " + loosingTradesCount);
+        System.out.println("Total trades: " + (winningTradesCount + loosingTradesCount));
+        System.out.println("Win rate: " + String.format("%.2f", winRate * 100) + "%");
+        System.out.println("Profit: " + String.format("%.2f", _profit) + " USD");
+        System.out.println("Trading volume: " + (winningTradesCount + loosingTradesCount) * tradeSize * leverage + " USD");
+        System.out.println("****************************************");
+
+        return allTrades;
+
+
+    }
+
+    public static void main(String[] args) {
+        String originalOpenClause = "WHERE rsi_14_ins_ <= 27";
+        String originalCloseClause = "WHERE rsi_14_ins_ >= 70";
+        String tableName = "btx";
+
+        // ---------------- Setup parameters ----------------
+        double balance = 5_000;
+        double leverage = 10;
+        double fee = (double) 2.5 / 1000;
+        double takeProfit = 0.08;
+        double stopLoss = 0.02;
+        double amount = 500;
+        double riskReward = (takeProfit) / (stopLoss);
+        int maxTrades = 10;
+        int delaySeconds = 3600;
+        int maxOpenedTrades = 0;
+        Setup setup = new Setup(balance, leverage, fee, takeProfit, stopLoss, amount, riskReward, maxTrades, delaySeconds);
+        // -------------------------------------------------
+
+
+        BacktestingExecutor backtestingExecutor = new BacktestingExecutor(originalOpenClause, originalCloseClause);
+        backtestingExecutor.createStrategyColumns(tableName);
+        backtestingExecutor.updateStrategyColumns(tableName);
+
+        backtestingExecutor.run(tableName, setup);
+
+
+    }
+
+}
 
 public class BackTestingDemo {
 
@@ -41,6 +388,24 @@ public class BackTestingDemo {
         return originalClause;
     }
 
+
+    // TODO - Needs to use class where can be some global state saved.
+    public static void tradeCloser(List<Trade> openTrades, double closePrice, String now) {
+
+        // close trade
+        for (Trade trade : openTrades) {
+
+            // 1) close if trade lifetime is greater than 2 days
+            if (isDifferenceGreaterThan(now, trade.openTime, 3600 * 24 * 2)) {
+                trade.closeTime = now;
+                trade.closePrice = closePrice;
+                openTrades.remove(trade);
+            }
+        }
+
+    }
+
+
     public static boolean isDifferenceGreaterThan(String dateString1, String dateString2, long n) {
         // Define the format for the date string that handles both formats (with and without microseconds)
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss[.SSSSSS]");
@@ -57,12 +422,7 @@ public class BackTestingDemo {
         return differenceInSeconds > n;
     }
 
-    public static void main(String[] args) throws SQLException, IOException {
-
-
-        // nějaké transformace
-        //TODO
-        // WHERE rsi:14 > 30 & rsi:14 < 70
+    public  void main(String[] args) throws SQLException, IOException {
 
 
         long t1 = System.currentTimeMillis();
@@ -136,19 +496,16 @@ public class BackTestingDemo {
         ResultSet rs = db.getResultSet(String.format("SELECT * FROM %s %s ORDER BY id", tableName, fromDate));
 
 
-        double balance = 40_00;
-        double leverage = 4;
+        double balance = 5_000;
+        double leverage = 10;
         double fee = (double) 2.5 / 1000;
         double takeProfit = 0.08;
-        double stopLoss = 0.05;
+        double stopLoss = 0.02;
         double amount = 500;
         double riskReward = (takeProfit) / (stopLoss);
-
         int maxTrades = 10;
         //int maxTrades = (int) ((int) balance / amount);
-
-        int delaySeconds = 1800;
-
+        int delaySeconds = 3600;
         int maxOpenedTrades = 0;
 
         System.out.println("\n****************************************");
@@ -193,11 +550,14 @@ public class BackTestingDemo {
                         tradeClosed = true;
                     }
 
-                    if (close &&  closePrice >= trade.openPrice * (1 + fee)) {
+                    // It is generally good to specify that:
+                    // - current close must be > than trade.open * (1 + takeProfit)
+                    // - current close must be > than trade.open * (1 + fee)
+                    if (close) {
                         tradeClosed = true;
                     }
 
-                    if (tradeClosed) {
+                    if (tradeClosed && closePrice >= trade.openPrice * (1 + fee)) {
                         trade.closeTime = rs.getString("date");
                         trade.closePrice = closePrice;
                         trade.PnL = (trade.closePrice * trade.amount - trade.openPrice * trade.amount) * leverage * (1 - fee);
@@ -304,7 +664,7 @@ public class BackTestingDemo {
             // print % profit
             double profitPercent = (((trade.closePrice) / trade.openPrice) * 100) - 100;
             //System.out.println(trade);
-            //System.out.println("Profit: " + String.format("%.2f", profitPercent) + "%");
+            System.out.println("Profit: " + String.format("%.2f", profitPercent) + "%");
         }
         double _profit = 0;
         for (Trade trade : allTrades) {
---
+++ src/main/java/com/github/sol239/javafi/backtesting_new/Setup.java
@@ -0,0 +1,45 @@
+package com.github.sol239.javafi.backtesting_new;
+
+public class Setup {
+    public double balance;
+    public double leverage;
+    public double fee;
+    public double takeProfit;
+    public double stopLoss;
+    public double amount;
+    public double riskReward;
+    public int maxTrades;
+    public int delaySeconds;
+
+    /**
+     * Stores the maximum number of opened trades at the same time.
+     */
+    public int maxOpenedTrades;
+
+    public Setup(double balance, double leverage, double fee, double takeProfit, double stopLoss, double amount, double riskReward, int maxTrades, int delaySeconds) {
+        this.balance = balance;
+        this.leverage = leverage;
+        this.fee = fee;
+        this.takeProfit = takeProfit;
+        this.stopLoss = stopLoss;
+        this.amount = amount;
+        this.riskReward = riskReward;
+        this.maxTrades = maxTrades;
+        this.delaySeconds = delaySeconds;
+        this.maxOpenedTrades = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "balance = " + balance + "\n" +
+                "leverage = " + leverage + "\n" +
+                "fee = " + fee + "\n" +
+                "takeProfit = " + takeProfit + "\n" +
+                "stopLoss = " + stopLoss + "\n" +
+                "amount = " + amount + "\n" +
+                "riskReward = " + riskReward + "\n" +
+                "maxTrades = " + maxTrades + "\n" +
+                "delaySeconds = " + delaySeconds + "\n" +
+                "maxOpenedTrades = " + maxOpenedTrades + "\n";
+    }
+}
