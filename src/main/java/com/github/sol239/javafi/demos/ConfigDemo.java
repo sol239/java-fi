a6ee2b873e4310658232ccf638ca4add27343d0d
David Valek <david.valek17@gmail.com>
2025-03-31 11:20:19 +0200

    Modify confg demo file

---
+++ src/main/java/com/github/sol239/javafi/demos/ConfigDemo.java
@@ -0,0 +1,33 @@
+package com.github.sol239.javafi.demos;
+
+import com.github.sol239.javafi.utils.files.ConfigHandler;
+
+/**
+ * Class for demonstrating the ConfigHandler class.
+ * Caution: the execution of this class will rewrite the config file.
+ */
+public class ConfigDemo {
+
+    public static void main(String[] args) {
+
+
+        // creates a new config file
+        ConfigHandler cf = new ConfigHandler();
+        cf.createConfigFile();
+
+        cf.url = "jdbc:postgresql://localhost:5432/javafi";
+        cf.password = "password";
+        cf.username = "postgres";
+
+        // fills the config map with the Config Handler class fields
+        cf.fillConfigMap();
+
+        // writes the config map to the config file
+        cf.writeConfigMap();
+
+        // loads the config map from the config file
+        ConfigHandler cfg = new ConfigHandler();
+        cfg.loadConfigMap();
+        cfg.printConfigMap();
+    }
+}
